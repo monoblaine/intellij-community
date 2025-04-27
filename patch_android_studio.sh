2977b4d85453da6dd4b8899b65f5a5116832b2de
Serhan Apaydın <zerhan@gmail.com>
2025-04-27 17:00:04 +0300

    refactor

--- patch_android_studio.sh
+++ patch_android_studio.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
 
-# https://plugins.jetbrains.com/docs/intellij/android-studio-releases-list.html
-#
-# Android Studio Meerkat 2024.3.1 Patch 2
-#
-# IntelliJ IDEA Version:
-# 2024.3.3
-# 243.24978.46
-
 path_to_product="C:/Program Files/Android/Android Studio"
-target_tag=$1
 
 net session >/dev/null 2>&1
 error_level=$?
 
 if [[ $error_level -ne 0 ]]; then
     echo Please run MSYS2 as administrator.
-    exit 1
-fi
-
-if [ -z "$target_tag" ]; then
-    echo '"target tag" argument (e.g. idea/243.24978.46) is required.'
     exit 1
 fi
 
@@ -33,10 +19,6 @@ jar () {
     "C:/Program Files/Java/jdk-17/bin/jar.exe" "$@"
 }
 
-git fetch origin refs/tags/$target_tag:refs/tags/$target_tag &&
-cd android &&
-git fetch origin refs/tags/$target_tag:refs/tags/$target_tag &&
-cd .. &&
 echo -e "
 1. Open project in Android Studio
 2. cherry-pick the commits
