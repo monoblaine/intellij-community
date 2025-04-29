303a3b90c62cf9fdec89710c2f038aea6a8594a8
Serhan Apaydın <zerhan@gmail.com>
2025-04-29 23:39:08 +0300

    Re&place => &Replace

--- platform/lang-impl/src/com/intellij/find/EditorSearchSession.java
+++ platform/lang-impl/src/com/intellij/find/EditorSearchSession.java
@@ -690,7 +690,7 @@ public class EditorSearchSession implements SearchSession,
 
   private final class ReplaceAction extends ButtonAction implements LightEditCompatible {
     ReplaceAction() {
-      super(ApplicationBundle.message("editorsearch.replace.action.text"), 'p');
+      super(ApplicationBundle.message("editorsearch.replace.action.text"), 'R');
     }
 
     @Override
