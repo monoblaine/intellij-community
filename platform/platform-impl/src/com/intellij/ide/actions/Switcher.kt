4abdc287c2c0aa75585e2afd57cd1f80db2baafe
Serhan Apaydın <zerhan@gmail.com>
2025-04-29 14:10:05 +0300

    If we're showing the Switcher via ctrl+shift+tab make sure it gets displayed pinned

--- platform/platform-impl/src/com/intellij/ide/actions/Switcher.kt
+++ platform/platform-impl/src/com/intellij/ide/actions/Switcher.kt
@@ -161,12 +161,12 @@ object Switcher : BaseSwitcherAction(null) {
         this.preferredSize = JBDimension(0, 0)
         this.size = JBDimension(0, 0)
       }
-      onKeyRelease = SwitcherKeyReleaseListener(if (recent) null else event) { e ->
+      onKeyRelease = SwitcherKeyReleaseListener(if (!forward || recent) null else event) { e ->
         ActionUtil.performInputEventHandlerWithCallbacks(ActionUiKind.POPUP, ACTION_PLACE, e) {
           navigate(e)
         }
       }
-      pinned = !onKeyRelease.isEnabled
+      pinned = !forward || !onKeyRelease.isEnabled
       val onlyEdited = true == onlyEditedFiles
       speedSearch = if (recent && Registry.`is`("ide.recent.files.speed.search")) installOn(this) else null
       cbShowOnlyEditedFiles = if (!recent) null else JCheckBox(IdeBundle.message("recent.files.checkbox.label"))
