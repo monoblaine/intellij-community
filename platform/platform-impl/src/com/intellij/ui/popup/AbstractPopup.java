999dd9503ae9b328158c2c99c9b7d0b51db16ec0
Serhan Apaydın <zerhan@gmail.com>
2025-05-01 09:57:21 +0300

    Hide "Quick Documentation" popup on left, right, home and end as well

--- platform/platform-impl/src/com/intellij/ui/popup/AbstractPopup.java
+++ platform/platform-impl/src/com/intellij/ui/popup/AbstractPopup.java
@@ -2744,7 +2744,11 @@ public class AbstractPopup implements JBPopup, ScreenAreaConsumer, AlignedPopup 
           }
         }
       }
-      return keyKode == KeyEvent.VK_ESCAPE && e.getModifiers() == 0;
+      return keyKode == KeyEvent.VK_ESCAPE && e.getModifiers() == 0
+        || keyKode == KeyEvent.VK_LEFT
+        || keyKode == KeyEvent.VK_RIGHT
+        || keyKode == KeyEvent.VK_HOME
+        || keyKode == KeyEvent.VK_END;
     }
     return false;
   }
