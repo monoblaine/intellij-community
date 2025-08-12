d597db49ac9f9a9a63e1305a44e08955537b7474
Serhan Apaydın <zerhan@gmail.com>
2025-08-12 18:52:49 +0300

    Refactor patch/restore scripts

--- patch_android_studio.sh
+++ patch_android_studio.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-path_to_product="C:/Program Files/Android/Android Studio"
-
 net session >/dev/null 2>&1
 error_level=$?
 
@@ -10,19 +8,83 @@ if [[ $error_level -ne 0 ]]; then
     exit 1
 fi
 
-if [ -f "./.tmp/app.jar.orig" ]; then
-    echo "./.tmp/app.jar.orig file already exists. Please delete or move it to somewhere else."
+version=$1
+
+if [ -z "$version" ]; then
+    echo '"version" argument (e.g. 251.25410.109.2511.13665796) is required.'
+    exit 1
+fi
+
+abs_path () {
+    cygpath -m "$(realpath "$1")"
+}
+
+program_files=$(abs_path "$PROGRAMFILES") &&
+path_to_product="$program_files/Android/Android Studio" &&
+path_to_ver=$(abs_path "./.bak/$version") &&
+path_to_original="$path_to_ver/original" &&
+path_to_patched="$path_to_ver/patched" &&
+path_to_tmp=$(abs_path "./.tmp") &&
+path_to_unzipped="$path_to_tmp/unzipped" &&
+path_to_out=$(abs_path "./out/production") &&
+
+mkdir -p "$path_to_original" &&
+mkdir -p "$path_to_patched" &&
+
+if [ -z "$(find "$path_to_original" -maxdepth 0 -empty)" ]; then
+    echo "Directory ($path_to_original) is not empty, aborting."
     exit 1
 fi
 
 jar () {
-    "C:/Program Files/Java/jdk-17/bin/jar.exe" "$@"
+    "$program_files/Java/jdk-17/bin/jar.exe" "$@"
+}
+
+init_jar_stuff () {
+    jar_name=$1 &&
+    relative_path_to_origin_jar=$2 &&
+    mkdir -p "$path_to_unzipped" &&
+    origin_jar="$path_to_product/$relative_path_to_origin_jar/$jar_name" &&
+    backup_jar="$path_to_original/$jar_name" &&
+    patched_jar="$path_to_patched/$jar_name" &&
+    cp -v "$origin_jar" "$backup_jar" &&
+    cd "$path_to_unzipped" &&
+    echo "Unzipping $jar_name..." &&
+    jar -xf "$backup_jar" &&
+    cd - >/dev/null &&
+    echo "Copying modified class files..." &&
+    cd "$path_to_out"
+}
+
+cp_cls () {
+    arr=("$@") &&
+    dest=${arr[-1]} &&
+    unset arr[-1] &&
+    cp -v "${arr[@]}" "$path_to_unzipped/$dest"
+}
+
+finalize_jar_stuff () {
+    is_last_task=${1:-0} &&
+    cd - >/dev/null &&
+    cd "$path_to_unzipped" &&
+    if [ -f "$patched_jar" ]; then
+       rm -v "$patched_jar"
+    fi &&
+    echo "Creating the new $jar_name..." &&
+    jar -cf0 "$patched_jar" '*' &&
+    cp -v "$patched_jar" "$origin_jar" &&
+    cd - >/dev/null &&
+    if [[ "$is_last_task" == "1" ]]; then
+        echo "Cleaning up..."
+    fi &&
+    rm -rf "$path_to_unzipped"
 }
 
 echo -e "
 1. Open project in Android Studio
 2. cherry-pick the commits
 3. Assemble the following modules:
+    * intellij.platform.analysis.impl
     * intellij.platform.lang.impl
     * intellij.platform.ide
     * intellij.platform.ide.impl
@@ -30,82 +92,51 @@ echo -e "
 4. Close Android Studio
 5. Press any key to continue" &&
 read -p "waiting..." &&
-if [ -d "./.tmp" ]; then
-    rm -rf ./.tmp
+if [ -d "$path_to_tmp" ]; then
+    rm -rf "$path_to_tmp"
 fi &&
-mkdir ./.tmp &&
-echo "*" >./.tmp/.gitignore &&
+mkdir -p "$path_to_tmp" &&
 
 # ==============================================================================
 # app.jar
 # ==============================================================================
-mkdir -p ./.tmp/unzipped &&
-echo "Creating a copy of app.jar..." &&
-cp "$path_to_product/lib/app.jar" ./.tmp/app.jar.orig &&
-cd ./.tmp/unzipped &&
-echo "Unzipping app.jar..." &&
-jar -xf ../app.jar.orig &&
-cd - >/dev/null &&
-echo "Copying modified class files..." &&
-cp "out/production/intellij.platform.ide.impl/com/intellij/ide/util/EditorGotoLineNumberDialog.class" \
-   ".tmp/unzipped/com/intellij/ide/util/EditorGotoLineNumberDialog.class" &&
-cp "out/production/intellij.platform.ide.impl/com/intellij/ide/actions/EditSourceInNewWindowAction.class" \
-   ".tmp/unzipped/com/intellij/ide/actions/EditSourceInNewWindowAction.class" &&
-cp 'out/production/intellij.platform.ide.impl/com/intellij/ide/actions/Switcher$SwitcherPanel.class' \
-   '.tmp/unzipped/com/intellij/ide/actions/Switcher$SwitcherPanel.class' &&
-cp "out/production/intellij.platform.lang.impl/com/intellij/codeInsight/highlighting/BraceHighlightingHandler.class" \
-   ".tmp/unzipped/com/intellij/codeInsight/highlighting/BraceHighlightingHandler.class" &&
-cp 'out/production/intellij.platform.lang.impl/com/intellij/find/EditorSearchSession$ReplaceAction.class' \
-   '.tmp/unzipped/com/intellij/find/EditorSearchSession$ReplaceAction.class' &&
-cp "out/production/intellij.platform.ide.impl/com/intellij/ui/popup/AbstractPopup.class" \
-   ".tmp/unzipped/com/intellij/ui/popup/AbstractPopup.class" &&
-cp "out/production/intellij.platform.ide.impl/com/intellij/openapi/fileEditor/impl/tabActions/CloseTab.class" \
-   ".tmp/unzipped/com/intellij/openapi/fileEditor/impl/tabActions/CloseTab.class" &&
-cp "out/production/intellij.platform.ide.impl/com/intellij/openapi/fileEditor/impl/tabActions/DotIcon.class" \
-   ".tmp/unzipped/com/intellij/openapi/fileEditor/impl/tabActions/DotIcon.class" &&
-cp 'out/production/intellij.platform.ide/com/intellij/ui/tabs/impl/JBTabsImpl$DefaultDecorator.class' \
-   '.tmp/unzipped/com/intellij/ui/tabs/impl/JBTabsImpl$DefaultDecorator.class' &&
-cp 'out/production/intellij.platform.ide/com/intellij/ui/tabs/impl/TabLabel.class' \
-   '.tmp/unzipped/com/intellij/ui/tabs/impl/TabLabel.class' &&
-cp 'out/production/intellij.platform.ide/com/intellij/ui/tabs/impl/JBDefaultTabPainter.class' \
-   '.tmp/unzipped/com/intellij/ui/tabs/impl/JBDefaultTabPainter.class' &&
-cp "out/production/intellij.platform.analysis.impl/messages/FindBundle.properties" \
-   ".tmp/unzipped/messages/FindBundle.properties" &&
-cp "out/production/intellij.platform.ide/messages/EditorBundle.properties" \
-   ".tmp/unzipped/messages/EditorBundle.properties" &&
-cd ./.tmp/unzipped &&
-if [ -f ../app.jar ]; then
-   rm ../app.jar
-fi &&
-echo "Creating the new app.jar..." &&
-jar -cf0 ../app.jar * &&
-echo "Moving the new app.jar to installation dir..." &&
-mv ../app.jar "$path_to_product/lib/app.jar" &&
-cd - >/dev/null &&
-rm -rf ./.tmp/unzipped &&
-
+init_jar_stuff "app.jar" "lib" &&
+# intellij.platform.analysis.impl
+cp_cls 'intellij.platform.analysis.impl/messages/FindBundle.properties' \
+       'messages/FindBundle.properties' &&
+# intellij.platform.lang.impl
+cp_cls 'intellij.platform.lang.impl/com/intellij/codeInsight/highlighting/BraceHighlightingHandler.class' \
+       'com/intellij/codeInsight/highlighting/BraceHighlightingHandler.class' &&
+cp_cls 'intellij.platform.lang.impl/com/intellij/find/EditorSearchSession$ReplaceAction.class' \
+       'com/intellij/find/EditorSearchSession$ReplaceAction.class' &&
+# intellij.platform.ide
+cp_cls 'intellij.platform.ide/com/intellij/ui/tabs/impl/JBTabsImpl$DefaultDecorator.class' \
+       'com/intellij/ui/tabs/impl/JBTabsImpl$DefaultDecorator.class' &&
+cp_cls 'intellij.platform.ide/com/intellij/ui/tabs/impl/TabLabel.class' \
+       'com/intellij/ui/tabs/impl/TabLabel.class' &&
+cp_cls 'intellij.platform.ide/com/intellij/ui/tabs/impl/JBDefaultTabPainter.class' \
+       'com/intellij/ui/tabs/impl/JBDefaultTabPainter.class' &&
+cp_cls 'intellij.platform.ide/messages/EditorBundle.properties' \
+       'messages/EditorBundle.properties' &&
+# intellij.platform.ide.impl
+cp_cls 'intellij.platform.ide.impl/com/intellij/ide/util/EditorGotoLineNumberDialog.class' \
+       'com/intellij/ide/util/EditorGotoLineNumberDialog.class' &&
+cp_cls 'intellij.platform.ide.impl/com/intellij/ide/actions/EditSourceInNewWindowAction.class' \
+       'com/intellij/ide/actions/EditSourceInNewWindowAction.class' &&
+cp_cls 'intellij.platform.ide.impl/com/intellij/ide/actions/Switcher$SwitcherPanel.class' \
+       'com/intellij/ide/actions/Switcher$SwitcherPanel.class' &&
+cp_cls 'intellij.platform.ide.impl/com/intellij/ui/popup/AbstractPopup.class' \
+       'com/intellij/ui/popup/AbstractPopup.class' &&
+cp_cls 'intellij.platform.ide.impl/com/intellij/openapi/fileEditor/impl/tabActions/CloseTab.class' \
+       'com/intellij/openapi/fileEditor/impl/tabActions/CloseTab.class' &&
+cp_cls 'intellij.platform.ide.impl/com/intellij/openapi/fileEditor/impl/tabActions/DotIcon.class' \
+       'com/intellij/openapi/fileEditor/impl/tabActions/DotIcon.class' &&
+finalize_jar_stuff &&
 # ==============================================================================
 # intellij.platform.recentFiles.frontend.jar
 # ==============================================================================
-mkdir -p ./.tmp/unzipped &&
-echo "Creating a copy of intellij.platform.recentFiles.frontend.jar..." &&
-cp "$path_to_product/lib/modules/intellij.platform.recentFiles.frontend.jar" ./.tmp/intellij.platform.recentFiles.frontend.jar.orig &&
-cd ./.tmp/unzipped &&
-echo "Unzipping intellij.platform.recentFiles.frontend.jar..." &&
-jar -xf ../intellij.platform.recentFiles.frontend.jar.orig &&
-cd - >/dev/null &&
-echo "Copying modified class files..." &&
-cp out/production/intellij.platform.recentFiles.frontend/com/intellij/platform/recentFiles/frontend/*.class \
-   ./.tmp/unzipped/com/intellij/platform/recentFiles/frontend/ &&
-cd ./.tmp/unzipped &&
-if [ -f ../intellij.platform.recentFiles.frontend.jar ]; then
-   rm ../intellij.platform.recentFiles.frontend.jar
-fi &&
-echo "Creating the new intellij.platform.recentFiles.frontend.jar..." &&
-jar -cf0 ../intellij.platform.recentFiles.frontend.jar * &&
-echo "Moving the new intellij.platform.recentFiles.frontend.jar to installation dir..." &&
-mv ../intellij.platform.recentFiles.frontend.jar "$path_to_product/lib/modules/intellij.platform.recentFiles.frontend.jar" &&
-cd - >/dev/null &&
-
-echo "Cleaning up..." &&
-rm -rf ./.tmp/unzipped
+init_jar_stuff "intellij.platform.recentFiles.frontend.jar" "lib/modules" &&
+# intellij.platform.recentFiles.frontend
+cp_cls intellij.platform.recentFiles.frontend/com/intellij/platform/recentFiles/frontend/*.class \
+       'com/intellij/platform/recentFiles/frontend/' &&
+finalize_jar_stuff 1
--- restore_android_studio.sh
+++ restore_android_studio.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-path_to_product="C:/Program Files/Android/Android Studio"
-
 net session >/dev/null 2>&1
 error_level=$?
 
@@ -10,4 +8,17 @@ if [[ $error_level -ne 0 ]]; then
     exit 1
 fi
 
-cp .tmp/app.jar.orig "$path_to_product/lib/app.jar"
+if [ -z "$1" ]; then
+    echo '"version" argument (e.g. 251.25410.109.2511.13665796) is required.'
+    exit 1
+fi
+
+abs_path () {
+    cygpath -m "$(realpath "$1")"
+}
+
+program_files=$(abs_path "$PROGRAMFILES") &&
+path_to_product="$program_files/Android/Android Studio" &&
+source_dir="./.bak/$1/original" &&
+cp -v "$source_dir/app.jar" "$path_to_product/lib/" &&
+cp -v "$source_dir/intellij.platform.recentFiles.frontend.jar" "$path_to_product/lib/modules/"
